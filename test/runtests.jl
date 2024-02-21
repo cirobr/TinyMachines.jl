8e174eae62c0630f8fd9780393bb89a80929a27d
cirobr <ciro.rosa@alumni.usp.br>
2024-02-21 16:12:58 -0300

    activations passed

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,12 +4,12 @@ using Random
 using Test
 
 Random.seed!(1234)
+x  = randn(Float32, (8,8,1,1))
 x1 = randn(Float32, (256,256,1,1))
 x3 = randn(Float32, (256,256,3,1))
-x  = randn(Float32, (8,8,1,1))
 
 @testset "TinyMachines.jl" begin
-    # include("./activations-tests.jl")
+    include("./activations-tests.jl")
     include("./convolutions-tests.jl")
     include("./unets-tests.jl")
 end
