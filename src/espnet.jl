cb6e276ba0de6389884d88cf8b7474b10f0a0c01
cirobr <ciro.rosa@alumni.usp.br>
2024-02-21 19:01:25 -0300

    prototype espnet.jl

---
+++ src/espnet.jl
@@ -0,0 +1,37 @@
+struct ESPnet
+    encoder::Chain
+    bridge::Chain
+    decoder::Chain
+end
+
+
+function ESPnet(ch_in::Int=3, ch_out::Int=1)
+    # encoder & bridge
+    e1 = Chain(ConvK3(ch_in, 16), BatchNorm(16, prelu2))
+    # concat(x, e1, dims=3) 19 channels out
+    br1 = ConvK1(19, ch_out)
+    # e2 = Chain(ESPmodule(19, 64), ESPmodule(64, 64; α=2))
+    # concat(x, e2[1], e2[2], dims=3) 131 channels out
+    br2 = ConvK1(131, ch_out)
+    # e3 = Chain(ESPmodule(131, 128), ESPmodule(128, 128; α=3))
+    # concat(e3[1], e3[2]) 256 channels out
+    br3 = ConvK1(256, ch_out)
+
+    # decoder
+
+
+    # output
+    encoder = Chain(e1, e2, e3)
+    bridge  = Chain(br1, br2, br3)
+    # decoder = 
+
+    return ESPnet(encoder, bridge, decoder)
+end
+
+
+function (m::ESPnet)(x::Array{Float32, 4})
+
+    return yhat
+end
+
+
