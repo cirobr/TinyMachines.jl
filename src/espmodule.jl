f1a3557dccbf2ff9ea77de8129fff1e702a47a34
cirobr <ciro.rosa@alumni.usp.br>
2024-02-23 12:11:17 -0300

    wip

--- src/espmodule.jl
+++ src/espmodule.jl
@@ -31,7 +31,7 @@ function (m::ESPmod)(x)
     pw = m.pointwise(x)
 
     # dilated convolutions
-    h, w, C = size(pw)[1:4]
+    h, w, C = size(pw)[1:3]
     dconvs = Array{Float32}(undef, (h, w, C, m.K))
     for i in 1:m.K   dconvs[:,:,:,i] = m.dilated[i](pw)   end
 
@@ -42,7 +42,6 @@ function (m::ESPmod)(x)
 
     # concatenate sums
     cat_sums = reshape(sums, (h, w, C*m.K, 1))
-    size(cat_sums)
 
     # sum concatenation with input tensor
 
@@ -57,3 +56,5 @@ c1  = tm.ConvK3(3,16; stride=2)(x)
 ds1 = tm.ConvK3(3,3;  stride=2)(x)
 ct1 = cat(c1, ds1, dims=3)        # 19ch OK
 yhat = ESPmod(19, 64, 4)(ct1)     # 64ch OK
+size(yhat)
+
