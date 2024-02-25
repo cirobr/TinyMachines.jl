69d88c884086e0a488b2f65a8d51bf1a83487c4e
cirobr <ciro.rosa@alumni.usp.br>
2024-02-25 13:51:19 +0000

    wip

--- src/pkgs/espmodule.jl
+++ src/pkgs/espmodule.jl
@@ -33,9 +33,8 @@ function (m::ESPmodule)(x)
 
     # dilated convolutions
     # h, w, C, N = size(pw)
-    # sums = pmap(i -> m.dilated[i](pw), 1:m.K)
-    sums = Vector{Array{Float32,4}}(undef, m.K)
-    map!(i -> m.dilated[i](pw), sums, 1:m.K)
+    sums = pmap(i -> m.dilated[i](pw), 1:m.K)
+    # sums = [m.dilated[i](pw) for i in 1:m.K]
     for i in 2:m.K   sums[i] += sums[i-1]   end
 
     # concatenate sums
