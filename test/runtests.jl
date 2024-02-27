cf82b5bc4b9aeef278bfec1c69d4991ef4e4bafc
cirobr <ciro.rosa@alumni.usp.br>
2024-02-27 12:22:07 -0300

    remove espnet

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,9 +8,9 @@ x1 = randn(Float32, (256,256,1,1))
 x3 = randn(Float32, (256,256,3,1))
 
 @testset "TinyMachines.jl" begin
-    include("./activations-tests.jl")
+    # include("./activations-tests.jl")
     include("./convolutions-tests.jl")   # TODO: DilatedConv
     include("./unets-tests.jl")
     include("./mobileunet-tests.jl")
-    include("./espnet-tests.jl")
+    # include("./espnet-tests.jl")
 end
