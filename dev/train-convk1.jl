376053335ca9577fee169eb77737cf201778c87c
cirobr <ciro.rosa@alumni.usp.br>
2024-02-29 13:09:46 +0000

    cleanup

--- dev/train-convk1.jl
+++ dev/train-convk1.jl
@@ -1,18 +1,16 @@
-using Pkg
-Pkg.activate( expanduser("~/envs/dev"))
+using TinyMachines
 using Flux
-using CUDA
+using Flux: DataLoader, gpu
 
-model = Flux.Conv((1,1), 3=>1) |> gpu
+X = rand(Float32, (256,256,3,1))
+Y = rand(Bool, (256,256,1,1))
+data = DataLoader((X,Y); batchsize=1)
+
+model = Conv((1,1), 3=>1, relu)
 
 loss(yhat, y) = Flux.mse(yhat, y)
 opt = Flux.Adam()
 optstate = Flux.setup(opt, model)
-
-X = rand(Float32, (128,128,3,1)) |> gpu
-s = size(model(X))
-Y = rand(Bool, s) |> gpu
-data = Flux.DataLoader((X,Y); batchsize=1)
 
 @time Flux.train!(model, data, optstate) do m,x,y
     loss(m(x), y)
--- dev/train-espmodule.jl
+++ dev/train-espmodule.jl
@@ -1,22 +1,17 @@
-using ESPNet
+using TinyMachines
 using Flux
-using Flux: @withprogress, DataLoader, gpu
-using CUDA
+using Flux: DataLoader, gpu
 
 X = rand(Float32, (256,256,3,1))
 Y = rand(Bool, (256,256,4,1))
-data = DataLoader((X,Y); batchsize=1) |> gpu
+data = DataLoader((X,Y); batchsize=1)
 
-model = ESPmodule(3, 4; K=4) |> gpu
+model = ESPmoduleK4(3, 4)
 
 loss(yhat, y) = Flux.mse(yhat, y)
 opt = Flux.Adam()
 optstate = Flux.setup(opt, model)
 
-@show ps=sum([length(v) for v in vec.(Flux.params(model))])
-
-# gpu 0.17s gpu
 @time Flux.train!(model, data, optstate) do m,x,y
     loss(m(x), y)
 end
-
