43d93ff62b042daa30463824e1661188286bbb54
cirobr <ciro.rosa@alumni.usp.br>
2024-03-04 15:08:07 +0000

    up

--- dev/prelu.jl
+++ dev/prelu.jl
@@ -1,16 +1,28 @@
 using Flux
-using Flux: Dense, relu, DataLoader, Conv
+using Flux: DataLoader
 
-X = rand(Float32, (256,256,3,1))
+struct prelu
+    alpha::Vector
+end
+
+function (m::prelu)(x)
+    a = m.alpha[1]
+    return a * Flux.relu(x)
+end
+Flux.@functor prelu
+
+model=prelu([1])
+Flux.trainable(model.alpha)
+Flux.params(model)
+
+loss(yhat, y) = Flux.mse(yhat, y)
+opt = Flux.Adam()
+optstate = Flux.setup(opt, model)
+
+X = rand(Float32, (256,256,1,1))
 Y = rand(Bool, (256,256,1,1))
 data = DataLoader((X,Y); batchsize=1)
 
-prelu3 = Conv((1,1), 1=>1, relu, bias=false, init=ones)
-opt = Flux.Adam()
-optstate = Flux.setup(opt, prelu3)
-
-
-loss(yhat, y) = Flux.mse(yhat, y)
-@time Flux.train!(prelu3, data, optstate) do m,x,y
+@time Flux.train!(model, data, optstate) do m,x,y
     loss(m(x), y)
 end
