b91709ec09e8f7985d084c502829370dacd4dfb7
cirobr <ciro.rosa@alumni.usp.br>
2024-03-11 02:38:01 +0000

    update

--- src/espnet-src/prelu.jl
+++ src/espnet-src/prelu.jl
@@ -1,12 +1,12 @@
-fn_prelu(x, alpha) = x>0 ? x : alpha * x
-
 struct model_prelu
     alpha::Vector{Float32}
 end
+Flux.@layer model_prelu
+
+fn_prelu(x, α) = x>0 ? x : α * x
 
 function (m::model_prelu)(x)
     return fn_prelu.(x, m.alpha[1])
 end
-Flux.@functor model_prelu
 
 prelu=model_prelu([0.f0])
