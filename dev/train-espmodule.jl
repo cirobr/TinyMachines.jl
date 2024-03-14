0eb4455fedd7dab4f5c7ab98751bd3f1a15964bf
cirobr <ciro.rosa@alumni.usp.br>
2024-03-14 10:31:13 +0000

    update

--- dev/train-espmodule.jl
+++ dev/train-espmodule.jl
@@ -2,18 +2,6 @@ using TinyMachines
 using Flux
 using BenchmarkTools
 
-X = rand(Float32, (256,256,3,1))
-Y = rand(Bool, (256,256,4,1))
-data = Flux.DataLoader((X,Y); batchsize=1)
-
-model = ESPmoduleK4(3, 4)
-@btime model(X) samples=5 seconds=15 gcsample=true
-
-
-# loss(yhat, y) = Flux.mse(yhat, y)
-# opt = Flux.Adam()
-# optstate = Flux.setup(opt, model)
-
-# @btime Flux.train!(model, data, optstate) do m,x,y
-#     loss(m(x), y)
-# end samples=1 seconds=15 gcsample=true
+X = rand(Float32, (256,256,64,1))
+model = ESPmoduleK4(64,64)
+@btime model(X) samples=5 seconds=15 gcsample=true;
--- dev/train-espnet.jl
+++ dev/train-espnet.jl
@@ -7,13 +7,14 @@ Y = rand(Bool, (256,256,1,1))
 data = Flux.DataLoader((X,Y); batchsize=1)
 
 model = ESPNet(3,1; activation=relu)
+model(X)
 @btime model(X) samples=5 seconds=15 gcsample=true;
 
 
-# loss(yhat, y) = Flux.mse(yhat, y)
-# opt = Flux.Adam()
-# optstate = Flux.setup(opt, model)
+loss(yhat, y) = Flux.mse(yhat, y)
+opt = Flux.Adam()
+optstate = Flux.setup(opt, model)
 
-# @btime Flux.train!(model, data, optstate) do m,x,y
-#     loss(m(x), y)
-# end samples=5 seconds=60 gcsample=true
+@btime Flux.train!(model, data, optstate) do m,x,y
+    loss(m(x), y)
+end samples=5 seconds=60 gcsample=true
