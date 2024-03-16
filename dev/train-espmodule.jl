19cf7ae5ce546c6403792e16640052df4895aacc
cirobr <ciro.rosa@alumni.usp.br>
2024-03-16 18:32:56 -0300

    benchmarks moved to a different project

--- dev/train-espmodule.jl
+++
@@ -1,7 +0,0 @@
-using TinyMachines
-using Flux
-using BenchmarkTools
-
-X = rand(Float32, (256,256,64,1))
-model = ESPmoduleK4(64,64)
-@btime model(X) samples=5 seconds=15 gcsample=true;
--- dev/train-irblock.jl
+++
@@ -1,7 +0,0 @@
-using TinyMachines
-using Flux
-using BenchmarkTools
-
-X = rand(Float32, (256,256,3,1))
-model = TinyMachines.irb2(3,3,1)   # ch_in, ch_out, expand_ratio
-@btime model(X) samples=5 seconds=15 gcsample=true
