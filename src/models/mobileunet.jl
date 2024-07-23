3034fc72005e985d43ce1e356d69953401faa714
cirobr <ciro.rosa@alumni.usp.br>
2024-07-23 16:38:55 -0300

    d5-do-025

--- src/models/mobileunet.jl
+++ src/models/mobileunet.jl
@@ -11,10 +11,10 @@ function MobileUNet(ch_in::Int=3, ch_out::Int=1;   # input/output channels
                     verbose::Bool = false,         # output feature maps
 )
     # encoder
-    dp = 0.2
+    dp = 0.25
     d1 = Chain(ConvK3(ch_in, 32, stride=2), BatchNorm(32, relu6),
-            #    Dropout(dp),
             n_irblock1(32, 16, n=1, expansion_factor=1),
+            #    Dropout(dp),
     )
 
     d2 = Chain(n_irblock2(16, 24, n=2, expansion_factor=6),
@@ -23,11 +23,11 @@ function MobileUNet(ch_in::Int=3, ch_out::Int=1;   # input/output channels
 
     d3 = Chain(n_irblock2(24, 32, n=3, expansion_factor=6),
                 # Dropout(dp),
-     )
+    )
 
     d4 = Chain(n_irblock2(32, 64, n=4, expansion_factor=6),
-                # Dropout(dp),
                 n_irblock1(64, 96, n=3, expansion_factor=6),
+                # Dropout(dp),
     )
 
     d5 = Chain(n_irblock2(96, 160, n=3, expansion_factor=6),
