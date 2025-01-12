8db08b89f24b69844beeb73094b6d0c27b47d9af
cirobr <ciro.rosa@alumni.usp.br>
2025-01-12 16:00:38 -0300

    cleanup

--- src/functions/unetblocks.jl
+++ src/functions/unetblocks.jl
@@ -15,21 +15,16 @@ Flux layers accept a single input and return a single output.
 The `UpBlock` struct is a wrapper around the `ConvTranspK2` layer.
 Its objective is to create a layer with two inputs and a single output.
 """
-# struct UpBlock
-#     u
-# end
-# @layer UpBlock trainable=(u)
+struct UpBlock
+    u
+end
+@layer UpBlock trainable=(u)
 
-# function UpBlock(ch_in::Int, ch_out::Int, activation)
-#     return UpBlock( ConvTranspK2(ch_in, ch_out, activation; stride=2) )
-# end
+function UpBlock(ch_in::Int, ch_out::Int, activation=identity)
+    return UpBlock( ConvTranspK2(ch_in, ch_out, activation; stride=2) )
+end
 
-# function (m::UpBlock)(dwn, skip)
-#     x = m.u(dwn)
-#     return cat(x, skip, dims=3)
-# end
-
-# cat4 = m.upc[:u4](enc5, enc4)
-# cat3 = m.upc[:u3](dec4, enc3)
-# cat2 = m.upc[:u2](dec3, enc2)
-# cat1 = m.upc[:u1](dec2, enc1)
+function (m::UpBlock)(dwn, skip)
+    x_up = m.u(dwn)
+    return cat(x_up, skip, dims=3)
+end
