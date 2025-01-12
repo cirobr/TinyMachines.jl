36745c0635f6ff621af3286673a410271658721d
cirobr <ciro.rosa@alumni.usp.br>
2025-01-12 13:44:59 -0300

    functor up block

--- src/functions/unetblocks.jl
+++ src/functions/unetblocks.jl
@@ -11,3 +11,20 @@ mcb(ch_in, ch_out, act) = Chain(MaxPoolK2,
 
 # up convolutional block
 ub(ch_in, ch_out, act)  = ConvTranspK2(ch_in, ch_out, act; stride=2)
+
+
+
+### up block
+struct UpBlock
+    u
+end
+@layer UpBlock trainable=(u)
+
+function UpBlock(ch_in::Int, ch_out::Int, act)
+    return UpBlock( ConvTranspK2(ch_in, ch_out, act; stride=2) )
+end
+
+function (m::UpBlock)(x, enc)
+    x = m.u(x)
+    return cat(x, enc, dims=3)
+end
