561d5ef927fa6c39cb1a3092ba0395c4caed8554
cirobr <ciro.rosa@alumni.usp.br>
2025-01-22 09:44:34 -0300

    explicit bn, act

--- src/functions/unetblocks.jl
+++ src/functions/unetblocks.jl
@@ -1,13 +1,13 @@
 # convolutional block
 CBlock(ch_in, ch_out, activation) = 
-    Chain(ConvK3(ch_in, ch_out, activation),
-          ConvK3(ch_out, ch_out), BatchNorm(ch_out, activation)
+    Chain(ConvK3(ch_in, ch_out), x -> activation.(x),
+          ConvK3(ch_out, ch_out), BatchNorm(ch_out), x -> activation.(x)
 )
 
 
 # maxpooling + convolutional block
 MCBlock(ch_in, ch_out, activation) = 
     Chain(MaxPool((2,2); stride=2),
-          ConvK3(ch_in, ch_out, activation),
-          ConvK3(ch_out, ch_out), BatchNorm(ch_out, activation)
+          ConvK3(ch_in, ch_out), x -> activation.(x),
+          ConvK3(ch_out, ch_out), BatchNorm(ch_out), x -> activation.(x)
 )
