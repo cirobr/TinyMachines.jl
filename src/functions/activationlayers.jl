efd85a0fde6e2c97798eff92fff1daa7bfefb813
cirobr <ciro.rosa@alumni.usp.br>
2025-05-18 18:25:36 +0000

    cleanup convprelu

--- src/functions/activationlayers.jl
+++ src/functions/activationlayers.jl
@@ -1,9 +1,8 @@
 # PReLU
-DwConvPrelu(ch_in::Int, activation::Function=identity) =
-    Chain(DepthwiseConv((1, 1), ch_in => ch_in, activation;
-                        bias=false,
-                        init=rand32)
-    )
+preluweights(ch_in::Int) = Chain(DepthwiseConv((1, 1), ch_in => ch_in;
+                                 bias=false,
+                                 init=rand32
+))
 
 struct ConvPReLU
     conv::Chain
@@ -11,9 +10,9 @@ end
 @layer ConvPReLU
 
 function ConvPReLU(ch_in::Int)
-    return ConvPReLU(DwConvPrelu(ch_in, identity))
+    return ConvPReLU(preluweights(ch_in))
 end
 
 function (m::ConvPReLU)(x)
-    return max.(x, 0) + m.conv(min.(x, 0))
+    return max.(x, 0) .+ m.conv(min.(x, 0))
 end
