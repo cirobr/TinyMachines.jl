7110d9465b085288261db6bc00a25b8d44345267
cirobr <ciro.rosa@alumni.usp.br>
2025-06-08 11:40:03 -0300

    cleanup

--- test/epoch-tests.jl
+++ test/epoch-tests.jl
@@ -5,14 +5,6 @@ Xs = randn(Float32, 64,64,3,5)
 ys = rand(Bool, 64,64,2,5)
 data = Flux.DataLoader((Xs, ys))
 
-# instantiate models
-# models = [
-#     ESPNet(3,2),
-#     MobileUNet(3,2),
-#     UNet4(3,2),
-#     UNet5(3,2),
-# ]
-
 dict = Dict{Any, String}(
     ESPNet(3,2)     => "ESPNet",
     MobileUNet(3,2) => "MobileUNet",
@@ -21,9 +13,8 @@ dict = Dict{Any, String}(
 )
 
 # training
-for (k,v) in dict
-    model = k
-    @info "... training $v"
+for (model, modelname) in dict
+    @info "... training $modelname"
     loss(model,x,y) = Flux.crossentropy(model(x), y; dims=3)
     opt = Flux.Adam()
     opt_state = Flux.setup(opt, model)
