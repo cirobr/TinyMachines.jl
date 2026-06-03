ac0f659d4addb9669cdd549fe871aeb9e2ccf3af
cirobr <ciro.rosa@alumni.usp.br>
2026-06-03 15:58:31 -0300

    changed prelu variable name

--- src/functions/activationlayers.jl
+++ src/functions/activationlayers.jl
@@ -1,13 +1,13 @@
 struct PReLU
-    vector::AbstractArray
+    weights::AbstractArray
 end
 @layer PReLU
 
 function PReLU(ch::Int)
-    vector = rand32(1,1,ch)
-    return PReLU(vector)
+    weights = rand32(1,1,ch)
+    return PReLU(weights)
 end
 
 function (m::PReLU)(x)
-    return fpos.(x) .+ (m.vector .* fneg.(x))
+    return fpos.(x) .+ (m.weights .* fneg.(x))
 end
